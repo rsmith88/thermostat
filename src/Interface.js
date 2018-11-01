cfd6c6ea3849ae067c92d387ecc112ba82d48051
Raylene <smith.raylene@gmail.com>
2018-11-01 17:04:54 +0000

    Added in dropdown menu and form to change city

--- src/Interface.js
+++ src/Interface.js
@@ -2,6 +2,26 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemp();
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+  });
+
+  $('#current-city').change(function() {
+  var city = $('#current-city').val();
+  console.log(city);
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp)
+    });
+  });
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  })
+
   $('#temp-up').click(function() {
     thermostat.increaseTemp();
     updateTemp();
